9d967c4a0f7f752f4fbb7282eefebf807180c6f8
Aykut ACIKGOZ <aykut@yela.com>
2022-02-14 13:20:12 +0300

    Implement Gloria's landing page

--- lib/main.dart
+++ lib/main.dart
@@ -13,28 +13,143 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider<AppStateBloc>(
-          create: (_) => AppStateBloc(),
-        ),
-        BlocProvider<ThemeBloc>(
-          create: (_) => ThemeBloc(),
-        ),
-      ],
-      child: BlocBuilder<ThemeBloc, AppTheme>(
-        builder: (context, theme) => MaterialApp(
-          title: 'Flutter Demo',
-          theme: ThemeData(
-            scaffoldBackgroundColor: theme.scaffoldColor,
-            primarySwatch: Colors.blue,
-          ),
-          home: const MyHomePage(),
-        ),
-      ),
-    );
+    return MaterialApp(
+        title: 'Flutter Demo',
+        home: Builder(builder: (BuildContext context) {
+          final size = MediaQuery.of(context).size;
+          return Scaffold(
+              backgroundColor: Colors.white,
+              body: Stack(
+                children: [
+                  Image.asset(
+                    'assets/background.png',
+                    fit: BoxFit.cover,
+                    height: double.infinity,
+                    width: double.infinity,
+                  ),
+                  SingleChildScrollView(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 96),
+                      child: Column(
+                        children: [
+                          const SizedBox(height: 48),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Image.asset(
+                                'assets/gloria_icon.png',
+                                width: 80,
+                              ),
+                              const Text(
+                                'Work at Gloria',
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.w300,
+                                    fontSize: 18),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 96),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Flexible(
+                                flex: 6,
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    const Text(
+                                      'The new platform for all football lovers. Now in beta on iOS.',
+                                      style: TextStyle(
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.w900,
+                                          fontSize: 32),
+                                    ),
+                                    const SizedBox(height: 40),
+                                    const Text(
+                                      'Chat about your favorite players and teams. Watch awesome videos. Show your skills and share your passion for the game. All in one place.',
+                                      style: TextStyle(
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.w500,
+                                          fontSize: 24),
+                                    ),
+                                    const SizedBox(height: 24),
+                                    ElevatedButton(
+                                      style: ElevatedButton.styleFrom(
+                                        primary: Colors.blue,
+                                        padding: const EdgeInsets.symmetric(
+                                          horizontal: 40,
+                                          vertical: 30,
+                                        ),
+                                        shape: RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(50.0),
+                                        ),
+                                        textStyle: const TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.w300,
+                                        ),
+                                      ),
+                                      child: const Text(
+                                          'Get access via TestFlight'),
+                                      onPressed: () => print('pressed'),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Flexible(flex: 1, child: Container()),
+                              Flexible(
+                                flex: 4,
+                                child: Padding(
+                                  padding: EdgeInsetsDirectional.only(
+                                      end: (size.width * 0.025)),
+                                  child: Image.asset(
+                                    'assets/hero_image.png',
+                                    fit: BoxFit.contain,
+                                    height: size.height * 1.2,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ));
+        }));
   }
 }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MultiBlocProvider(
+//       providers: [
+//         BlocProvider<AppStateBloc>(
+//           create: (_) => AppStateBloc(),
+//         ),
+//         BlocProvider<ThemeBloc>(
+//           create: (_) => ThemeBloc(),
+//         ),
+//       ],
+//       child: BlocBuilder<ThemeBloc, AppTheme>(
+//         builder: (context, theme) => MaterialApp(
+//           title: 'Flutter Demo',
+//           theme: ThemeData(
+//             scaffoldBackgroundColor: theme.scaffoldColor,
+//             primarySwatch: Colors.blue,
+//           ),
+//           home: const MyHomePage(),
+//         ),
+//       ),
+//     );
+//   }
+// }
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({Key? key}) : super(key: key);
