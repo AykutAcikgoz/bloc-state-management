6fd100bf5423499a79a6876a656092b96b858959
Aykut ACIKGOZ <aykut@yela.com>
2022-02-14 13:33:25 +0300

    Update title and favicon

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
+        title: 'Gloria – The World\'s Football Community',
         home: Builder(builder: (BuildContext context) {
           final size = MediaQuery.of(context).size;
           return Scaffold(
